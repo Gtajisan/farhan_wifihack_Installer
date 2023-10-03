bfe06893f79888fabd8a22922e7d2d266ada619a
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2023-10-03 08:10:11 +0600

    Add files via upload

---
+++ Installer.py
@@ -0,0 +1,13 @@
+import os
+print('''\033[1;36;40mfarhan_wifihack Installer By THBD
+Your Device Must Be Rooted
+If Any Question,
+Contact Me On Telegram
+Tg_User:@Farhan\n''')
+os.system("pkg install -y root-repo")
+os.system("pkg install -y git tsu python wpa-supplicant pixiewps iw")
+os.system("cd ..;git clone https://github.com/gtajisan/farhan_wifihack")
+
+os.system("cd ..;chmod +x farhan_wifihack/farhan.py")
+
+print("\033[1;34;40mThanks.\nInstallation Done.\nNow Go To Home Directory[~] And Enter This Command :\nsudo python farhan_wifihack/farhan.py -i wlan0 -K")

b63f16ac4da11b4fcd631c183b5b255e88c290d0
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2023-10-14 09:47:06 +0600

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -1,13 +1,9 @@
-import os
-print('''\033[1;36;40mfarhan_wifihack Installer By FARHAN
-Your Device Must Be Rooted
-If Any Question,
-Contact Me On Telegram
-Tg_User:@Farhan\n''')
-os.system("pkg install -y root-repo")
-os.system("pkg install -y git tsu python wpa-supplicant pixiewps iw")
-os.system("cd ..;git clone https://github.com/gtajisan/farhan_wifihack")
+#!/data/data/com.termux/files/usr/bin/bash
+pkg install -y root-repo 
+pkg install -y git tsu python wpa-supplicant pixiewps iw
 
-os.system("cd ..;chmod +x farhan_wifihack/farhan.py")
+git clone --depth 1 https://github.com/Gtajisan/farhan_wifihack
 
-print("\033[1;34;40mThanks.\nInstallation Done.\nNow Go To Home Directory[~] And Enter This Command :\nsudo python farhan_wifihack/farhan.py -i wlan0 -K")
+chmod +x farhan_wifihack/farhan_wifihack.py
+
+printf "###############################################\n#  All done! Now you can run farhan_wifihack with\n#   sudo python farhan_wifihack.py/farhan_wifihack.py -i wlan0 -K\n#\n#  To update, run\n#   (cd farhan_wifihack.py && git pull)\n###############################################\n"

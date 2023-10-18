94f1ceb2a5a680b78f85f88ec5386204d571b7b9
FARHAN MUH TASIM <124022055+Gtajisan@users.noreply.github.com>
2023-10-18 18:48:19 +0600

    Update Installer.py

--- Installer.py
+++ Installer.py
@@ -2,8 +2,8 @@
 pkg install -y root-repo 
 pkg install -y git tsu python wpa-supplicant pixiewps iw
 
-git clone --depth 1 https://github.com/Gtajisan/farhan_wifihack
+git clone --depth 1 https://github.com/gtajisan/farhan_wifihack farhan_wifihack
 
 chmod +x farhan_wifihack/farhan_wifihack.py
 
-printf "###############################################\n#  All done! Now you can run farhan_wifihack with\n#   sudo python farhan_wifihack.py/farhan_wifihack.py -i wlan0 -K\n#\n#  To update, run\n#   (cd farhan_wifihack.py && git pull)\n###############################################\n"
+printf "###############################################\n#  All done! Now you can run farhan_wifihack with\n#   sudo python farhan_wifihack/farhan_wifihack.py -i wlan0 --iface-down -K\n#\n#  To update, run\n#   (cd farhan_wifihack && git pull)\n###############################################\n"
